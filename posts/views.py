fb34994b72b652cf42c1002fe40686ccf697fb69
jinju <jeongjinju7031@gmail.com>
2019-10-23 11:53:00 +0900

    Post model migrate

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from .forms import PostForm
 # Create your views here.
 
 def index(request):
     return render(request, 'posts/index.html')
 
 def create(request):
-    pass
+    if request.method == "POST":
+        pass
+    else:
+        form = PostForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'posts/form.html', context)
