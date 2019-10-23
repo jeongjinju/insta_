f522c3d848d98c4dce5087b42dd01485aa97597d
jinju <jeongjinju7031@gmail.com>
2019-10-23 11:16:54 +0900

    follow 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -49,3 +49,40 @@ def user_page(request, id):
     }
 
     return render(request, 'accounts/user_page.html', context)
+
+def follow(request, id):
+    # 팔로우 하려는 사람, 팔로우 당하는 사람 구분 주의!
+    
+    # # 이미 내가 팔로우 한 상황에서 취소
+    # if you.followers.filter(id=me.id):
+    #     you.followers.remove(me)
+    # else:
+    #     you.followers.add(me)
+    
+    # return redirect('accounts:user_page', you.id)
+
+
+    # if me.followings.filter(id=you.id):
+    #     me.followings.remove(you)
+    # else:
+    #     me.followings.add(you
+    #     )
+    # return redirect('accounts:user_page', you.id)
+
+    #----같은 역할을 하는 코드
+
+    # 팔로우 당하는 사람, 팔로잉
+    you = get_object_or_404(User, id=id)
+    # 지금 로그인 한 사람, 팔로우 하려는 사람, 팔로워
+    me = request.user
+
+    if you != me:
+        if me in you.followings.all():
+            you.followings.remove(me)
+        else:
+            you.followings.add(me)
+    return redirect('accounts:user_page', you.id)
+
+
+
+
